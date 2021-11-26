03af7dd57a9a13ded4eeeb2efdfce174307e22f6
Franco <franco.mana@hotmail.com>
2021-11-26 15:07:45 -0300

    UI Ammon, Health

--- Assets/Scripts/Work/GameManager.cs
+++ Assets/Scripts/Work/GameManager.cs
@@ -1,17 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class GameManager : MonoBehaviour
 {
+    public Text ammoText;
+    public Text healthText;
+
     //patron singleton
     public static GameManager Instance { get; private set; }
 
     public int gunAmmon = 10;
+    public int health = 100;
+
+
 
     private void Awake()
     {
         Instance = this;
+    }
+
+    private void Update()
+    {
+        ammoText.text = gunAmmon.ToString();
+        healthText.text = health.ToString();
     }
 
 }
